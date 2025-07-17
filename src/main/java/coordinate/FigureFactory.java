19915af40371733435cc492cfde234ce92e502b3
lkwoung88 <lkwoung88@gmail.com>
2025-07-17 21:44:40 +0900

    feat : FigureCreator 인터페이스 구현

--- src/main/java/coordinate/FigureFactory.java
+++ src/main/java/coordinate/FigureFactory.java
@@ -1,21 +1,33 @@
 package coordinate;
 
 import java.util.List;
+import java.util.Map;
 
-public class FigureFactory {
-    static Figure getInstance(List<Point> points) {
-        if (points.size() == Line.LINE_POINT_SIZE) {
-            return new Line(points);
+public class FigureFactory implements FigureCreator {
+
+    private Map<Integer, Class<? extends Figure>> figureMap = Map.of(
+            Line.LINE_POINT_SIZE, Line.class,
+            Triangle.TRIANGLE_POINT_SIZE, Triangle.class,
+            Rectangle.RECTANGLE_POINT_SIZE, Rectangle.class
+    );
+
+    private FigureFactory() {}
+
+    private static class Holder {
+        private static final FigureFactory INSTANCE = new FigureFactory();
+    }
+
+    public static Figure getInstance(List<Point> points) {
+        return Holder.INSTANCE.create(points);
+    }
+
+    @Override
+    public Figure create(List<Point> points) {
+        Class<? extends Figure> figureClass = figureMap.get(points.size());
+        try {
+            return figureClass.getConstructor(List.class).newInstance(points);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("도형 생성 중 오류가 발생했습니다.", e);
         }
-
-        if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
-            return new Triangle(points);
-        }
-
-        if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
-            return new Rectangle(points);
-        }
-
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
     }
 }
